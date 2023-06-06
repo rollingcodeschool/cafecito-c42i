b8745df59da2ec89d8edbabeb898796b23ef9da7
Emilse Arias <emilsearias@gmail.com>
2023-06-06 18:02:52 -0300

    agregamos el link al componente Menu

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,11 +1,11 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
-
+import {Link, NavLink} from 'react-router-dom'
 
 const Menu = () => {
   return (
     <Navbar bg="danger" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Cafecito</Navbar.Brand>
+        <Navbar.Brand as={Link} to='/'>Cafecito</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
