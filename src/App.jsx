297a9ddd21ec7a136fc11a4d7c0ab5f33aee010e
Yanina Rodriguez <rodriguezyanina169@gmail.com>
2023-06-07 17:55:07 -0300

    modificacion de links de menu

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,31 @@ import Error404 from "./components/views/Error404";
 import Menu from "./components/common/Menu";
 import Footer from "./components/common/Footer";
 import Inicio from "./components/views/Inicio";
+import Login from "./components/views/Login";
 import DetalleProducto from "./components/views/DetalleProducto";
 import CrearProducto from "./components/views/producto/CrearProducto";
 import EditarProducto from "./components/views/producto/EditarProducto";
 import Administrador from "./components/views/Administrador";
+import { BrowserRouter, Routes,Route } from "react-router-dom";
+import Registro from "./components/views/Registro"
 
 function App() {
   return (
     <>
+      <BrowserRouter>
       <Menu></Menu>
-      {/* <Error404></Error404> */}
-      {/* <Inicio></Inicio> */}
-      {/* <DetalleProducto></DetalleProducto> */}
-      {/* <CrearProducto></CrearProducto> */}
-      {/* <EditarProducto></EditarProducto> */}
-      <Administrador></Administrador>
+      <Routes>
+        <Route exact path="/" element={<Inicio></Inicio>}></Route>
+        <Route exact path="/registro" element={<Registro></Registro>}></Route>
+        <Route exact path="/login" element={<Login></Login>}></Route>
+        <Route exact path="/detalle" element={<DetalleProducto></DetalleProducto>}></Route>
+        <Route exact path="/administrador" element={<Administrador></Administrador>}></Route>
+        <Route exact path="/administrador/crear-producto" element={<CrearProducto></CrearProducto>}></Route>
+        <Route exact path="/administrador/editar-producto" element={<EditarProducto></EditarProducto>}></Route>
+        <Route path="*" element={<Error404></Error404>}></Route>
+      </Routes>
       <Footer></Footer>
+      </BrowserRouter>
     </>
   );
 }
