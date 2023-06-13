2c47e589bbd46b07b521e21fa7b426026acd944a
Emilse Arias <emilsearias@gmail.com>
2023-06-13 18:29:15 -0300

    agregamos la logica del logout

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 
 function App() {
-  const [usuarioLogueado, setUsuarioLogueado] = useState({});
+  const usuarioSessionStorage = JSON.parse(sessionStorage.getItem('usuario')) || {}
+  const [usuarioLogueado, setUsuarioLogueado] = useState(usuarioSessionStorage);
 
   return (
     <BrowserRouter>
--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,7 +1,14 @@
 import { Navbar, Container, Nav, Button } from "react-bootstrap";
-import {Link, NavLink} from 'react-router-dom'
+import {Link, NavLink, useNavigate} from 'react-router-dom'
 
 const Menu = ({usuarioLogueado, setUsuarioLogueado}) => {
+  const navegacion = useNavigate();
+
+  const cerrarSesion = ()=>{
+    sessionStorage.removeItem('usuario');
+    setUsuarioLogueado({});
+    navegacion('/')
+  }
   return (
     <Navbar bg="danger" variant="dark" expand="lg">
       <Container>
@@ -15,7 +22,7 @@ const Menu = ({usuarioLogueado, setUsuarioLogueado}) => {
               (usuarioLogueado.nombreUsuario)?
               <>
               <NavLink end className='nav-item nav-link' to='/administrador'>Administrador</NavLink>
-              <Button variant="dark">Logout</Button>
+              <Button variant="dark" onClick={cerrarSesion}>Logout</Button>
               </>:<NavLink end className='nav-item nav-link' to='/login'>Login</NavLink>
             }
           </Nav>
