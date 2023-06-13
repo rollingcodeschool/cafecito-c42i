913a0a5333430d2b5750cb88f5a6dfa16c9fe6d1
Emilse Arias <emilsearias@gmail.com>
2023-06-13 16:45:09 -0300

    agregamos validaciones al input del email del formulario de login

--- src/components/views/Login.jsx
+++ src/components/views/Login.jsx
@@ -1,22 +1,37 @@
 
 import { Form, Button, Container, Card } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 
 const Login = () => {
+  const { register, handleSubmit, formState: {errors}, reset} = useForm(); 
+
+  const onSubmit = ( usuario)=>{
+    console.log(usuario);
+  }
+
   return (
     <Container className="mainSection">
       <Card className="my-5">
         <Card.Header as="h5">Login</Card.Header>
         <Card.Body>
-          <Form >
+          <Form onSubmit={handleSubmit(onSubmit)}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email</Form.Label>
               <Form.Control
-                type="email"
+                type="text"
                 placeholder="Ingrese un email"
-               
+               { ...register('email', {
+                required:'El email es un dato obligatorio',
+                pattern:{
+                  value: /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=? ^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a -z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+                  message:'El email debe tener el siguiente formato mail@dominio.com'
+                }
+               } ) }
               />
-             
+             <Form.Text className="text-danger">
+               {errors.email?.message}
+             </Form.Text>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
